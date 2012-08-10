86a996fa945ba3fdb28af9d526b77b0448acf9bd
George Stephanis <george@stephanis.info>
2012-08-10 11:31:49 -0400

    Modifying and migrating install script

--- app/code/community/Stephanis/Pagemeta/sql/pagemeta_setup/mysql-install-0.1.0.php
+++ app/code/community/Stephanis/Pagemeta/sql/pagemeta_setup/mysql-install-0.1.0.php
@@ -1,13 +1,13 @@
 <?php
 
 $installer = $this;
-$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
 $installer->startSetup();
  
 $installer->run("
 
--- DROP TABLE IF EXISTS {$this->getTable('pagemeta')};
-CREATE TABLE IF NOT EXISTS {$this->getTable('pagemeta')} (
+-- DROP TABLE IF EXISTS {$this->getTable('pagemeta/pagemeta')};
+CREATE TABLE IF NOT EXISTS {$this->getTable('pagemeta/pagemeta')} (
 	`pagemeta_id` int(11) unsigned NOT NULL auto_increment,
 	`page_id` int(11) unsigned NOT NULL,
 	`key` varchar(255) NOT NULL,
